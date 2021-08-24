56b5299b046a4e79010e7d69c3f92236de25a409
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-08-23 20:35:39 -0400

    Add help messages for the two arguments

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "sm-pattern-gen",
-    about = "Program for generating .sm file format streams of a given snap."
+    about = "Program for generating .sm file format patterns of a given snap."
 )]
 struct Opt {
+    #[structopt(
+        help = "Specifies the snap for the given pattern. Acceptable snaps are: 4th, 8th, 12th, 16th, 20th, 22nd, 26th, 28th, 32nd, 36th, 40th, 44th, 48th, 64th, 70th, 80th, 96th, and 192nd."
+    )]
     snap: Snap,
+    #[structopt(
+        help = "Specifies the pattern. Acceptable patterns are: stream, light jumpstream, jumpstream, handstream, and chordjacks."
+    )]
     pattern: Pattern,
 }
 fn main() {
